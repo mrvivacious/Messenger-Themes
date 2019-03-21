65ceb7398b88a38a9882450fa2df1ad382ff628b
linuszhu1031 <linuszhu1031@gmail.com>
2019-03-20 19:52:26 -0500

    Some files previously made yeehaw

---
+++ themes.js
@@ -0,0 +1,30 @@
+// themes.js
+// this fileis responsible for the coloring 
+
+// step 1
+// grab the ID's of Messenger's chat bubbles, text, and background
+
+window.onload = () => {
+setTimeout(doNothing, 5000);
+
+let bubbles = document.getElementsByClassName('_3058 _ui9 _hh7 _6ybn _s1- _52mr _43by _3oh-');
+alert('num of bubbles = ' + bubbles.length);
+
+for (let i = 0; i < bubbles.length; i++) {
+bubbles[i].style.backgroundColor = 'lightgreen'
+}
+
+
+// step 2
+// modify the color
+
+// step 3
+
+}
+
+function doNothing() {
+// does nothing
+let i = 0;
+while (i < 1999)
+i++;
+}
