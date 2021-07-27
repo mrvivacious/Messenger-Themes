6adf566ea7d1f93009e6fcd7b95f998078bb9035
Vivek Bhookya <v.bhookya98@gmail.com>
2021-07-27 02:13:23 -0500

    random commit

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -7,14 +7,14 @@
 // CHAT: Where the messages in an opened conversation sit
 // DIVIDER: The vertical line between the list of all conversations and
 //  the currently opened conversation
-const BACKGROUND_PANEL_TOP = '_673w';
-const BACKGROUND_PANEL_LEFT = '_1enh';
-const BACKGROUND_PANEL_CHAT = '_5irm';
-const BACKGROUND_PANEL_DIVIDER = '_4sp8';
-const BACKGROUND_PANEL_BOTTOM = '_4rv3 _7og6';
+const BACKGROUND_PANEL_TOP = 'rq0escxv l9j0dhe7 du4w35lb j83agx80 pfnyh3mw i1fnvgqd bp9cbjyn owycx6da btwxx1t3 hv4rvrfc dati1w0a f10w8fjw pybr56ya b5q2rw42 lq239pai mysgfdmx hddg9phg';
+const BACKGROUND_PANEL_LEFT = 'hybvsw6c cjfnh4rs j83agx80 cbu4d94t dp1hu0rb l9j0dhe7 kr520xx4 iyyx5f41 jyyn76af ow71b3p4 pnfry9he so2p5rfc owhy4gn4 wrtwqps9';
+const BACKGROUND_PANEL_CHAT = 'gok29vw1';
+const BACKGROUND_PANEL_DIVIDER = 'kd0sc8dh sl8jk4me ie5zihkj i09qtzwb rm3jng1j hzruof5a pmk7jnqg kr520xx4 c0wkt4kp';
+const BACKGROUND_PANEL_BOTTOM = 'j83agx80 l9j0dhe7 aovydwv3 ni8dbmo4 stjgntxs nred35xi n8tt0mok hyh9befq';
 
 // Messages
-const ALL_MESSAGES = '_s1-';
+const ALL_MESSAGES = 'l60d2q6s d1544ag0 sj5x9vvc tw6a2znq l9j0dhe7 ni8dbmo4 stjgntxs qlfml3jp';
 const OUR_MESSAGES = '_43by';
 const THEIR_MESSAGES = '_3oh-';
 const REPLY_MESSAGE = '_6uum';
