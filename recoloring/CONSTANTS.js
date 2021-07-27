dc2055cc0520e6cd3950f27ba742ac10afd8fc34
Vivek Bhookya <v.bhookya98@gmail.com>
2021-07-27 07:30:20 -0500

    [Road to mvp] Updated background recoloring

---
+++ recoloring/CONSTANTS.js
@@ -0,0 +1,44 @@
+// Classes
+
+// Background
+// TOP: Top of the chat window, where the recipient's profile picture and
+//  name sit
+// LEFT: "Chats" panel, where all the conversations sit
+// CHAT: Where the messages in an opened conversation sit
+// DIVIDER: The vertical line between the list of all conversations and
+//  the currently opened conversation
+const BACKGROUND_PANEL_TOP =
+  "rq0escxv l9j0dhe7 du4w35lb j83agx80 pfnyh3mw i1fnvgqd bp9cbjyn owycx6da btwxx1t3 hv4rvrfc dati1w0a f10w8fjw pybr56ya b5q2rw42 lq239pai mysgfdmx hddg9phg";
+const BACKGROUND_PANEL_LEFT =
+  "hybvsw6c cjfnh4rs j83agx80 cbu4d94t dp1hu0rb l9j0dhe7 kr520xx4 o36gj0jk jyyn76af ow71b3p4 pnfry9he so2p5rfc owhy4gn4 wrtwqps9";
+const BACKGROUND_PANEL_CHAT = "gok29vw1";
+const BACKGROUND_PANEL_CHAT_LOADING =
+  "j83agx80 bp9cbjyn taijpn5t rgmg9uty rj1gh0hx buofh1pr pfnyh3mw pybr56ya f10w8fjw nred35xi";
+const BACKGROUND_PANEL_DIVIDER =
+  "kd0sc8dh sl8jk4me ie5zihkj i09qtzwb rm3jng1j hzruof5a pmk7jnqg kr520xx4 c0wkt4kp"; // no longer selectable in chrome insepct?
+const BACKGROUND_PANEL_BOTTOM =
+  "j83agx80 l9j0dhe7 aovydwv3 ni8dbmo4 stjgntxs nred35xi n8tt0mok hyh9befq";
+const BACKGROUND_PANEL_BOTTOM_WHEN_EXPOSED =
+  "nred35xi j83agx80 rv4hoivh cxgpxx05 k4urcfbm rq0escxv";
+const BACKGROUND_PANEL_RECIPIENT_DETAIL_PAGE =
+  "rq0escxv l9j0dhe7 du4w35lb j83agx80 cbu4d94t g5gj957u f4tghd1a ifue306u kuivcneq t63ysoy8";
+
+// Messages
+const ALL_MESSAGES =
+  "l60d2q6s d1544ag0 sj5x9vvc tw6a2znq l9j0dhe7 ni8dbmo4 stjgntxs qlfml3jp";
+const OUR_MESSAGES = "_43by";
+const THEIR_MESSAGES = "_3oh-";
+const REPLY_MESSAGE = "_6uum";
+const MESSAGES_WITH_REACTION = "_5z-5";
+
+// Misc
+// POPUP_MEDIA_PANEL: When the Messenger window is
+//  minimized to a particular width, the Upload image / Stickers / GIF / etc
+//  menu disappears -- we keep this to recolor the panel when it is toggled open
+const TYPING_INDICATOR = "clearfix _17pz";
+const MESSAGE_STATUS_INDICATOR = "_2her";
+const POPUP_MEDIA_PANEL = "_7mkk _7t1o _7t0e";
+const PAPER_AIRPLANE = "_30yy _38lh _7kpi";
+const SMILEY_FACE = "_30yy _7odb";
+
+const REPLIES = "_3egs";
